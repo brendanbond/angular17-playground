0e2b8ab31b4780fa71f3fb82692f7848e8887a37
Brendan J Bond <brendanbond@gmail.com>
2025-03-05 16:04:55 -0600

    add premium and library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormioModule } from '@formio/angular';
+import { Formio } from '@formio/js';
+import premium from '@formio/premium';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+Formio.license = 'myLicenseHere';
+Formio.use(premium);
 
 @NgModule({
   declarations: [
